df5407013cdcdfa4893c95cf5682276a88817e7c
Broxton <sayremouse3@gmail.com>
2024-12-09 14:14:39 -0700

    w?

--- features/spy_messages.feature
+++ features/spy_messages.feature
@@ -3,10 +3,24 @@ As a secret spy
 I should be able to encode and decode messages
 So that I can chat with my spy friends like a pro.
 
-Scenario: I can successfully encode a secret message
+
+Background:
     Given I open the url "https://www.hanginghyena.com/solvers_a/caesar-cipher-decoder"
-    # write your steps here
+
+Scenario: I can successfully encode a secret message
+    When I set "hello world" to the inputfield "#letters"
+    When I click on the element "#shift-amount"
+    When I select the option with the value "1" for element "#shift-amount"
+    When I click on the element "#submit"
+    When I pause for 1000ms
+    Then I expect that element "#decoded_message" contains the text "ifmmp xpsme"
 
 Scenario: I can successfully decode a secret message
-    Given I open the url "https://www.hanginghyena.com/solvers_a/caesar-cipher-decoder"
-    # write your steps here
+    When I set "ifmmp xpsme" to the inputfield "#letters"
+    When I click on the element "#shift-amount"
+    When I select the option with the value "1" for element "#shift-amount"
+    When I click on the element "#decoder-setting"
+    When I select the option with the value "D" for element "#decoder-setting"
+    When I click on the element "#submit"
+    When I pause for 1000ms
+    Then I expect that element "#decoded_message" contains the text "hello world"
--- test_brute.py
+++ test_brute.py
@@ -1,18 +1,37 @@
 import pytest
 from brute import Brute
 
-todo = pytest.mark.skip(reason='todo: pending spec')
-
 def describe_Brute():
-
     @pytest.fixture
     def cracker():
         return Brute("TDD")
 
+    # Tests for bruteOnce without test doubles
     def describe_bruteOnce():
-        # write your test cases here
-        pass
+        def it_succeeds_when_guess_is_correct(cracker):
+            assert cracker.bruteOnce("TDD") == True
 
+        def it_fails_when_guess_is_incorrect(cracker):
+            assert cracker.bruteOnce("wrong") == False
+
+    # Tests for bruteMany with test doubles
     def describe_bruteMany():
-        # write your test cases here
-        pass
+        def it_succeeds_when_correct_guess_is_made(mocker):
+            b = Brute("correct_guess")
+            mocker.patch.object(b, 'randomGuess', return_value="correct_guess")
+            assert b.bruteMany(100) > 0
+
+        def it_fails_when_no_correct_guess_is_made(mocker):
+            b = Brute("correct_guess")
+            mocker.patch.object(b, 'randomGuess', return_value="wrong_guess")
+            assert b.bruteMany(100) == -1
+
+        def it_verifies_method_calls(mocker):
+            b = Brute("correct_guess")
+            random_mock = mocker.patch.object(b, 'randomGuess', return_value="wrong_guess")
+            hash_mock = mocker.patch.object(b, 'hash', wraps=b.hash)
+
+            b.bruteMany(10)
+
+            random_mock.assert_called()
+            hash_mock.assert_called()
